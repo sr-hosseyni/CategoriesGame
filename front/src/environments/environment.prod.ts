270bed5ccd83bc2cb2f93db74deafc47d3ad88f1
rassoul hosseini <seyedrasoul.hosseini@real-digital.de>
2022-06-27 03:59:13 +0200

    angular build works alongside symfony api

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  basePath: 'https://rass.dev',
+  basePath: 'https://rass.dev/api',
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  basePath: 'http://localhost',
+  basePath: 'http://localhost/api',
 };
 
 /*
